5d1d04a7f28281abfdabe6c56a0fce265cd2a02b
Luís Arteiro <lucho14garfield@gmail.com>
2023-02-17 12:18:28 +0000

    feat: Adding tests.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,37 +6,62 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/main.dart';
+import 'package:todo_app/pages/login.dart';
+import 'package:todo_app/pages/signup.dart';
 
 import './widget_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<SupabaseClient>()])
 @GenerateNiceMocks([MockSpec<GoTrueClient>()])
 void main() {
-  testWidgets('Shows and redirects to home, since we have session', (WidgetTester tester) async {
+    testWidgets('Shows splash and redirects to home, since we have session', (WidgetTester tester) async {
+      // Mocking supabase variable with session
+      const Session mockSession =
+          Session(accessToken: "some_token", tokenType: "some_type", user: User(appMetadata: {}, aud: '', createdAt: '', id: '', userMetadata: {}));
 
-    // Mocking supabase variable with session
-    const Session mockSession =
-        Session(accessToken: "some_token", tokenType: "some_type", user: User(appMetadata: {}, aud: '', createdAt: '', id: '', userMetadata: {}));
+      MockSupabaseClient mockSupabase = MockSupabaseClient();
+      MockGoTrueClient mockTrueClient = MockGoTrueClient();
 
-    MockSupabaseClient mockSupabase = MockSupabaseClient();
-    MockGoTrueClient mockTrueClient = MockGoTrueClient();
+      when(mockTrueClient.currentSession).thenReturn(mockSession);
+      when(mockSupabase.auth).thenReturn(mockTrueClient);
 
-    when(mockTrueClient.currentSession).thenReturn(mockSession);
-    when(mockSupabase.auth).thenReturn(mockTrueClient);
+      await tester.pumpWidget(ProviderScope(
+          child: App(
+        supabase: mockSupabase,
+      )));
 
-    await tester.pumpWidget(ProviderScope(
-        child: App(
-      supabase: mockSupabase,
-    )));
+      // The first frame is a loading state.
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
-    // The first frame is a loading state.
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      // Re-render. TodoListProvider should have finished fetching the todos by now
+      await tester.pumpAndSettle();
+      await tester.pump(const Duration(milliseconds: 100));
 
-    // Re-render. TodoListProvider should have finished fetching the todos by now
-    await tester.pumpAndSettle();
-    await tester.pump(const Duration(milliseconds: 100));
+      // No longer loading
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
 
-    // No longer loading
-    expect(find.byType(CircularProgressIndicator), findsNothing);
-  });
+    testWidgets('Shows splash and redirects to login, since there\s no session', (WidgetTester tester) async {
+      // Mocking supabase variable with session
+      MockSupabaseClient mockSupabase = MockSupabaseClient();
+      MockGoTrueClient mockTrueClient = MockGoTrueClient();
+
+      when(mockTrueClient.currentSession).thenReturn(null);
+      when(mockSupabase.auth).thenReturn(mockTrueClient);
+
+      await tester.pumpWidget(ProviderScope(
+          child: App(
+        supabase: mockSupabase,
+      )));
+
+      // The first frame is a loading state.
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+      // Re-render. TodoListProvider should have finished fetching the todos by now
+      await tester.pumpAndSettle();
+      await tester.pump(const Duration(milliseconds: 100));
+
+      // Redirected to login page
+      expect(find.text("Login"), findsWidgets);
+    });
 }
